2c8ac53ab864dfa92eff432ecc92174267a08b1d
yangyang.ju <1198746549@qq.com>
2018-01-18 11:11:10 +0800

    Add:添加反爬策略增加 IProxy代理。

--- LPythonSpider/LPythonSpider/Util/common.py
+++ LPythonSpider/LPythonSpider/Util/common.py
@@ -2,17 +2,28 @@
 
 import requests
 import json
+import hashlib
+import requests
+import json
+import random
 
-class ProxiesHelper():
-    @classmethod
-    def queryIP(cls):
-        r = requests.get('http://127.0.0.1:8000/?types=0&count=5&country=国内')
-        ip_ports = json.loads(r.text)
-        print ip_ports
-        ip = ip_ports[0][0]
-        port = ip_ports[0][1]
-        proxies = {
-            'http': 'http://%s:%s' % (ip, port),
-            'https': 'http://%s:%s' % (ip, port)
-        }
-        return proxies
+
+def get_md5(url):
+    if isinstance(url, unicode):
+        url = url.encode('utf-8')
+    m = hashlib.md5(url)
+    m.update(url)
+    return m.hexdigest()
+
+def QueryRandomIP():
+    r = requests.get('http://127.0.0.1:8000/?types=0&count=20&country=国内')
+    ip_ports = json.loads(r.text)
+    index = random.randint(0, len(ip_ports) - 1)
+    ip = ip_ports[index][0]
+    port = ip_ports[index][1]
+    proxies = {
+        'http': 'http://%s:%s' % (ip, port),
+        'https': 'http://%s:%s' % (ip, port)
+    }
+    return proxies
+
